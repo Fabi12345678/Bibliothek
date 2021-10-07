270345bd6979b6ef95dda7e1359c50fb3979274d
Fabian <ventiks@web.de>
2021-10-07 09:49:22 +0200

    SQL COnnection Setup und Nutzer Statements

--- WebApplication3/SQLRequests.cs
+++ WebApplication3/SQLRequests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,17 @@ namespace WebApplication3
 {
     public class SQLRequests
     {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=.\SQLEXPRESS; AttachDbFilename=" + Properties.Settings.Default.connString + "; Integrated Security=True; Connect Timeout=30; User Instance=True");
+        
         public string LoadUsers()
         {
             string options = "";
-
-            // SQL shit
-
+            string cmd = "";
+            SqlCommand selectCommand = new SqlCommand(cmd, sqlConnection);
+            sqlConnection.Open();
+                // SQL shit
+                selectCommand.ExecuteNonQuery();
+            sqlConnection.Close();
             return options;
         }
     }
---
+++ WebApplication3/SQLStatements/Nutzer.sql
@@ -0,0 +1,36 @@
+﻿-- nutzerabfrage
+select n.name as nname, n.nummer as nnummer 
+from nutzer as n
+
+
+insert into nutzer
+values (1, 'Manuel Neuer', 5.55);
+
+insert into nutzer
+values (2, 'Otto Dumke', 0.00);
+
+insert into nutzer
+values (3, 'Otto Thiel', 12.45);
+
+insert into nutzer
+values (4, 'Willi Fick', 2143.00);
+
+insert into nutzer
+values (5, 'Johannes Schneider', 1.00);
+
+insert into nutzer
+values (6, 'Georg Krogmann', 1.87);
+
+insert into nutzer
+values (7, 'Erich Massini', 3.61);
+
+insert into nutzer
+values (8, 'Hans Schmidt', 3.14);
+
+insert into nutzer
+values (9, 'Karl Hanssen', 13.37);
+
+insert into nutzer
+values (10, 'Olaf Scholz', 10000000000.00);
+
+select * from nutzer
